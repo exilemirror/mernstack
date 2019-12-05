5f27fcdb19627a71180847768b7bf885df845f1d
Hasshim Ali <exilemirror@yahoo.com.sg>
2019-12-05 22:11:07 +0800

    Add routes

--- server.js
+++ server.js
@@ -9,5 +9,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Hello-World!'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => console.log('Server is running on ' + PORT));
